966b8af959d6371e7c974d25ed112ec146deb8e8
Surmach <curmachevskiy@gmail.com>
2020-02-04 23:39:28 +0200

    Updates

---
+++ precache-manifest.6d3fc2ab2cef296b6b40c0c37595b2f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be75cab1e4b39da0c7c721bd4c7979b4",
+    "url": "/Diplom/index.html"
+  },
+  {
+    "revision": "d281c9c841c60ee2d51c",
+    "url": "/Diplom/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "fede7e6ce8f88719b1ab",
+    "url": "/Diplom/static/css/main.50113c99.chunk.css"
+  },
+  {
+    "revision": "d281c9c841c60ee2d51c",
+    "url": "/Diplom/static/js/2.b76073ab.chunk.js"
+  },
+  {
+    "revision": "1fd42126ff6c2bce1661721eb32a2059",
+    "url": "/Diplom/static/js/2.b76073ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fede7e6ce8f88719b1ab",
+    "url": "/Diplom/static/js/main.7487f5b2.chunk.js"
+  },
+  {
+    "revision": "2f22605205e78e206871",
+    "url": "/Diplom/static/js/runtime-main.20e128c7.js"
+  }
+]);
